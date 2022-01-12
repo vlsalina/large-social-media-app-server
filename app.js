f4c1f3ed2c8e38416057371ed9ff5f3f5be1e6db
vlsalina <vlsalina@ucsd.edu>
2022-01-11 16:36:03 -0800

    Adding Access-Control-Allow headers to allow http requests to be made from localhost:3000.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
 require("./database");
 
 var indexRouter = require("./routes/index");
@@ -13,11 +12,6 @@ var repliesRouter = require("./routes/replies");
 var authRouter = require("./routes/auth");
 
 var app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
 
 const port = process.env.PORT || 5000;
 
@@ -30,6 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
