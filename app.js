f0de6f717766b5edfd4929f82f16bd1b120c93c2
vlsalina <vlsalina@ucsd.edu>
2022-03-09 14:55:19 -0800

    Fixed typo in Access-Allow-Origin-Header

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://large-social-media.netlify.app/"
+    "https://large-social-media.netlify.app"
   );
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
