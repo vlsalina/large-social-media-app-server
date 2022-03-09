6283bf010e3aa46d370cdeeff88e886508b3d8fc
vlsalina <vlsalina@ucsd.edu>
2022-03-09 14:51:35 -0800

    Changed Access-Allow-Origin-Header to allow frontend site access.

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://large-social-media.netlify.app/"
+  );
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
   next();
